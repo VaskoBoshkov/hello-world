8fa60783357f199ed8e971242dce565cfbba5b41
aleks <apetkov81@outlook.com>
2025-07-05 12:29:36 +0200

    new version deployment

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Change the text here to update your visible version
-app.MapGet("/", () => "Hello World version 3");
+app.MapGet("/", () => "Hello World version 4");
 
 app.Run();
