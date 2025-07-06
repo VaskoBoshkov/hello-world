3ba6832a1bbab674c5e1633432cd7f715b77f19c
acodexpetkov <56339072+acodexpetkov@users.noreply.github.com>
2025-07-06 15:27:52 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Change the text here to update your visible version
-app.MapGet("/", () => "Hello World version Final with ECS !!!!");
+app.MapGet("/", () => "Hello World version Final with ECS ver 2 !!!!");
 
 app.Run();
